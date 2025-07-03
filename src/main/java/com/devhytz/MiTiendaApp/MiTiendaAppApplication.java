33cd94dafde54d7d7b09e4f6d94ad1a1f96d6337
Alejandro <aledevrus70@gmail.com>
2025-07-03 17:18:55 -0500

    Inicio

---
+++ src/main/java/com/devhytz/MiTiendaApp/MiTiendaAppApplication.java
@@ -0,0 +1,13 @@
+package com.devhytz.MiTiendaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiTiendaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiTiendaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devhytz/MiTiendaApp/MiTiendaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhytz.MiTiendaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiTiendaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
