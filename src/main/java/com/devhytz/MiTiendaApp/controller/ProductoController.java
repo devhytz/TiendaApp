545d6cc9690d48cb14d608141406df3a107ae7c3
Alejandro <aledevrus70@gmail.com>
2025-07-03 19:52:48 -0500

    fix: error en metodos

--- src/main/java/com/devhytz/MiTiendaApp/controller/ProductoController.java
+++ src/main/java/com/devhytz/MiTiendaApp/controller/ProductoController.java
@@ -1,35 +1,30 @@
 package com.devhytz.mitiendaapp.controller;
 
 import com.devhytz.mitiendaapp.model.*;
+import com.devhytz.mitiendaapp.service.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductoController {
 
-    @GetMapping("/api/producto")
-    public ResponseEntity<Producto> crearObjeto() {
-        Producto productoCreado = new Producto("Camiseta", 29.99, 100);
-        return ResponseEntity.ok(productoCreado);
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+    this.productoService = productoService;
+}
+
+    @GetMapping("/api/producto/crear")
+    public void crearObjeto() {
+        Producto productoNuevo = new Producto("Camiseta", 29.99, 100);
+        productoService.guardarProducto(productoNuevo);
+
     }
 
     @GetMapping("/api/productos")
-    public ResponseEntity<List<Producto>> listaProductos() {
-        List<Producto> lista = new ArrayList<>();
-
-        Producto productoUno = new Producto("Camiseta", 29.99, 100);
-        Producto productoDos = new Producto("Pantalon", 39.99, 50);
-        Producto productoTres = new Producto("Zapatos", 99.99, 10);
-
-        lista.add(productoUno);
-        lista.add(productoDos);
-        lista.add(productoTres);
-
-        return ResponseEntity.ok(lista);
+    public void listaProductos() {
+        productoService.obtenerTodos();
     }
 }
